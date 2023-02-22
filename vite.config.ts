700194642aa3d4945b00b41cb0f829dd5976c7f1
李雨锋 <liyufeng@gz-yibo.com>
2023-02-22 23:15:50 +0800

    ci: 🎡 新增按需导入组件与API配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/@types/auto-imports.d.ts',
+      exclude: [/node_modules/],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      extensions: ['vue'],
+      dts: 'src/@types/components.d.ts',
+      exclude: [/node_modules/],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      '@': resolve(__dirname, 'src'),
     },
   },
   server: {
